68ad54bcb4474ce256b37a77353de6f2cee5f0ef
Tanguy Palmie <51063280+XDemonBloodX@users.noreply.github.com>
2021-03-03 16:24:57 +0100

    js cookie

---
+++ Webchill/frontend/js/policy.js
@@ -0,0 +1,24 @@
+cookieAgree = $(".cookJs");
+
+var cook = document.cookie.split(';');
+const ipCurrent = getCurrentIp();
+
+console.log(ipCurrent);
+if (cook[0] == "policy=accept:" + ipCurrent) {
+    cookieAgree.css('display', 'none');
+} else {
+    cookieAgree.css('display', 'block');
+}
+
+
+$("#spaceBtnPrivacy").click(function() {
+    console.log("l")
+    document.cookie = "policy=accept:" + ipCurrent;
+    cookieAgree.css('display', 'none');
+});
+
+function getCurrentIp() {
+    $.getJSON("https://api.ipify.org/?format=json", function(ip) {
+        return ip.ip;
+    });
+}
